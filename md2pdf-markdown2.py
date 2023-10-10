0b076b9063972fbfda0709fb8dd2806e6271dfda
hdaojin <hdaojin@hotmail.com>
2023-10-10 16:10:26 +0800

    version2.0 best for markdown2

--- md2pdf-markdown2.py
+++ md2pdf-markdown2.py
@@ -1,33 +1,46 @@
-# Markdown file coverted to html and pdf file with python-markdown2 and wkhtmltopdf
-# Author: Hdaojin
-# Date: 2021-12-05
-# Update: 2021-12-05
-__version__ = 1.0
-__author__ = 'Hdaojin'
-__date__ = '2021-12-05'
+"""
+Description: Markdown file coverted to html and pdf file with python-markdown2 and wkhtmltopdf
+Author: hdaojin
+Date: 2021-12-05
+Update: 2023-10-10
+Version: 2.0.0
+"""
 
-import markdown2
-from configparser import ConfigParser
-import argparse
 import os
 import sys
 from pathlib import Path
-from pathlib import PurePath
 from shutil import copy, rmtree
 from string import Template
+from configparser import ConfigParser
+
 import pdfkit
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
+import markdown2
+import click
+
+# from pygments import highlight
+# from pygments.lexers import PythonLexer
+# from pygments.formatters import HtmlFormatter
+
+# Get the directory of the current file
+DIRECTORY = Path(__file__).parent
+
+# template directory
+TEMPLATE_DIR = DIRECTORY / "templates"
+
+# styles directory
+STYLES_DIR = DIRECTORY / "styles"
+
+# Template file name
+TEMPLATE_FILE_NAME = "template.html"
+
+# Template css file name
+TEMPLATE_FILE_CSS = "template.css"
+
+# Config file
+CONFIG_FILE = DIRECTORY / "config.ini"
 
 
-TEMPLATE_DIR = Path('template')
-TEMPLATE_FILE_NAME = 'template.html'
-TEMPLATE_FILE_CSS = 'template.css'
-MDCSS_DIR = Path('styles')
-CONFIG_FILE = 'config.ini'
-
-
+# Get sections from config file, return a list, porvid the user to choose.
 def get_sections():
     cfg = ConfigParser()
     cfg.read(CONFIG_FILE)
@@ -35,145 +48,139 @@ def get_sections():
     return sections
 
 
+# Get config in the section wich user choose. It have decided the style of the html and pdf file.
 def get_config(section):
     cfg = ConfigParser()
     cfg.read(CONFIG_FILE)
-    template = cfg.get(section, 'template_name')
-    markdown_css = cfg.get(section, 'markdown_css')
-    custom_css = cfg.get(section, 'markdown_custom_css')
-    code_highlight_css = cfg.get(section, 'code_highlight_css')
-    generate_html = cfg.getboolean(section, 'generate_html')
-    generate_pdf = cfg.getboolean(section, 'generate_pdf')
-    return template, markdown_css, custom_css, code_highlight_css, generate_html, generate_pdf
+    config = dict(cfg.items(section))
+    return config
 
 
-def md2html(md_file, template, markdown_css, custom_css, code_highlight_css):
-    extensions = ['metadata', 'tables', 'code-friendly',
-                  'fenced-code-blocks', 'break-on-newline', 'footnotes']
-    markdown_css_path = os.path.join(MDCSS_DIR, markdown_css)
-    custom_css_path = os.path.join(MDCSS_DIR, custom_css)
-    code_highlight_css_path = os.path.join(MDCSS_DIR, code_highlight_css)
-    template_css_path = os.path.join(TEMPLATE_DIR, template, TEMPLATE_FILE_CSS)
-    template_path = os.path.join(TEMPLATE_DIR, template, TEMPLATE_FILE_NAME)
+# Convert markdown file to html file.
+def md2html(md_file, output_dir, config):
+    markdown_css_path = STYLES_DIR / config["markdown_css"]
+    markdown_custom_css_path = STYLES_DIR / config["markdown_custom_css"]
+    template_path = TEMPLATE_DIR / config["template_name"] / TEMPLATE_FILE_NAME
+    template_css_path = TEMPLATE_DIR / config["template_name"] / TEMPLATE_FILE_CSS
+
     md_file_dir = md_file.parent
-    html_file_name = md_file.stem + '.html'
+    styles_dir = md_file_dir / "styles"
+    html_file = os.path.join(md_file_dir, md_file.stem + ".html")
 
-    css_dir = os.path.join(md_file_dir, 'css')
-    if not os.path.exists(css_dir):
-        os.mkdir(css_dir)
-    for css in markdown_css_path, custom_css_path, code_highlight_css_path, template_css_path:
-        copy(css, css_dir)
-    markdown_css = os.path.join('css', markdown_css)
-    custom_css = os.path.join('css', custom_css)
-    code_highlight_css = os.path.join('css', code_highlight_css)
-    template_css = os.path.join('css', TEMPLATE_FILE_CSS)
+    if not Path.exists(styles_dir):
+        Path.mkdir(styles_dir)
 
-    with open(md_file, 'r', encoding="utf-8") as mf:
+    for style_file in markdown_css_path, markdown_custom_css_path, template_css_path:
+        copy(style_file, styles_dir)
+
+    extensions = [
+        "metadata",
+        "tables",
+        "code-friendly",
+        "fenced-code-blocks",
+        "break-on-newline",
+        "footnotes",
+    ]
+
+    with open(md_file, "r", encoding="utf-8") as mf:
         html = markdown2.markdown(mf.read(), extras=extensions)
-        title = html.metadata['Head']
-        author = html.metadata['Author']
-        role = html.metadata['Role']
-        date = html.metadata['Date']
-        subject = html.metadata['Subject']
-        task = html.metadata['Task']
-    with open(template_path, 'r', encoding="utf-8") as tf:
+        meta_data = html.metadata
+        meta_data = {k.lower(): v for k, v in meta_data.items()}
+
+    with open(template_path, "r", encoding="utf-8") as tf:
         template_html = Template(tf.read())
-        full_html = template_html.safe_substitute(
-            markdown_css=markdown_css,
-            custom_css=custom_css,
-            code_highlight_css=code_highlight_css,
-            template_css=template_css,
-            title=title,
-            author=author,
-            role=role,
-            date=date,
-            subject=subject,
-            task=task,
-            content=html)
-    html_file = os.path.join(md_file_dir, html_file_name)
-    with open(html_file, 'w', encoding="utf-8") as hf:
+        d = {**config, **meta_data}
+        full_html = template_html.safe_substitute(d, content=html)
+
+    with open(html_file, "w", encoding="utf-8") as hf:
         hf.write(full_html)
-    return title, author, role, date, subject, task, html_file, css_dir
+
+    return html_file, styles_dir, meta_data
 
 
+# Convert html file to pdf file.
 def html2pdf(html_file, pdf_file):
     options = {
-        'encoding': "UTF-8",
-        'margin-top': '15mm',
-        'margin-right': '15mm',
-        'margin-bottom': '15mm',
-        'margin-left': '15mm',
-        'page-size': 'A4',
-        'custom-header': [
-            ('Accept-Encoding', 'gzip')
+        "encoding": "UTF-8",
+        "margin-top": "15mm",
+        "margin-right": "15mm",
+        "margin-bottom": "15mm",
+        "margin-left": "15mm",
+        "page-size": "A4",
+        "custom-header": [("Accept-Encoding", "gzip")],
+        "cookie": [
+            ("cookie-empty-value", '""'),
+            ("cookie-name1", "cookie-value1"),
+            ("cookie-name2", "cookie-value2"),
         ],
-        'cookie': [
-            ('cookie-empty-value', '""'),
-            ('cookie-name1', 'cookie-value1'),
-            ('cookie-name2', 'cookie-value2'),
-        ],
-        'footer-font-size': '6',
-        'footer-font-name': 'Times-Roman',
-        'footer-center': '[page]/[topage]',
-        'footer-spacing': '5',
-        'enable-local-file-access': None
+        "footer-font-size": "6",
+        "footer-font-name": "Times-Roman",
+        "footer-center": "[page]/[topage]",
+        "footer-spacing": "5",
+        "enable-local-file-access": None,
     }
     pdfkit.from_file(html_file, pdf_file, options=options)
     return
 
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description="Markdown file coverted to html and pdf file with python-markdown2 and wkhtmltopdf",
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s {}'.format(str(__version__)))
-    parser.add_argument('-t', '--template', required=False, nargs='?', const='DEFAULT', default='DEFAULT',
-                        help='select a HTML template: {}\n(default: %(default)s)'.format(get_sections()))
-    parser.add_argument('-i', required=True,
-                        metavar='markdown_file', help='input a markdown file')
-    # parser.add_argument('-o', metavar='directory',
-    #                    default='.', help='output directory\n(default: The directory where markdown file is located)')
-    parser.add_argument('-o', metavar='directory',
-                        help='output directory\n(default: The directory where markdown file is located)')
-    args = parser.parse_args()
+# Get parameters from the command line using click
 
-    md_file = Path(args.i)
-    if not args.o:
-        output_dir = md_file.resolve().parent
+sections = get_sections()
+
+
+@click.command()
+@click.option(
+    "-t",
+    "--template",
+    default="DEFAULT",
+    show_default="True",
+    help=f"select a HTML template: {sections}",
+)
+@click.argument(
+    "md_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    required=True,
+    nargs=1,
+)
+@click.argument(
+    "output_dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+    required=False,
+    nargs=1,
+)
+def main(template, md_file, output_dir):
+    md_file = Path(md_file).resolve()
+    if not output_dir:
+        output_dir = md_file.parent
     else:
-        output_dir = args.o
-    output_dir = Path(output_dir)
-    template = args.template
+        output_dir = Path(output_dir).resolve()
+    if template not in sections and template != "DEFAULT":
+        print(f"Error: template {template} is not exist.")
+        sys.exit()
 
-    if not md_file.is_file():
-        print('Error: {} not found'.format(md_file))
-        sys.exit(1)
-    if not output_dir.is_dir():
-        output_dir.mkdir()
-    if template not in get_sections() and template != 'DEFAULT':
-        print('Warnning: template \'{}\' not found, use \'DEFAULT\''.format(template))
-        template = 'DEFAULT'
-    return md_file, output_dir, template
+    config = get_config(template)
+    html_file, styles_dir, meta_data = md2html(md_file, output_dir, config)
+    # pdf_file = output_dir / (md_file.stem + '.pdf')
+    name_parts = [
+        meta_data["document"],
+        meta_data["role"],
+        meta_data["date"],
+        meta_data["author"],
+        meta_data["subject"],
+        meta_data["task"],
+    ]
+    # pdf_file = output_dir / ('-'.join(name_parts) + '.pdf')
+    pdf_file = os.path.join(output_dir, "-".join(name_parts) + ".pdf")
 
+    if config["generate_pdf"]:
+        html2pdf(html_file, pdf_file)
 
-if __name__ == '__main__':
-    md_file, output_dir, template = get_args()
-    template, markdown_css, custom_css, code_highlight_css, generate_html, generate_pdf = get_config(
-        template)
-    title, author, role, date, subject, task, html_file, css_dir = md2html(
-        md_file, template, markdown_css, custom_css, code_highlight_css)
-
-    if generate_pdf == True:
-        # pdf_file_name = md_file.name.replace('.md', '.pdf')
-        # pdf_file_name = role + '-' + title + '-' + \
-        #     date + '-' + author + '-' + subject + '.pdf'
-        pdf_file_name = '-'.join([role, title, date, author, subject]) + '.pdf'
-        pdf_file = os.path.join(output_dir, pdf_file_name)
-        html2pdf(html_file=html_file, pdf_file=pdf_file)
-
-    if generate_html == False:
-        print('Delete ', html_file)
+    if config["generate_html"] == "False":
+        print(f"Delete {html_file}")
         os.remove(html_file)
-        print('Delete ', css_dir)
-        rmtree(css_dir)
+        print(f"Delete {styles_dir}")
+        rmtree(styles_dir)
+
+
+if __name__ == "__main__":
+    main()
--- md2pdf.py
+++ md2pdf.py
@@ -25,19 +25,19 @@ import click
 DIRECTORY = Path(__file__).parent
 
 # template directory
-TEMPLATE_DIR = DIRECTORY / 'templates'
+TEMPLATE_DIR = DIRECTORY / "templates"
 
 # styles directory
-STYLES_DIR = DIRECTORY / 'styles'
+STYLES_DIR = DIRECTORY / "styles"
 
 # Template file name
-TEMPLATE_FILE_NAME = 'template.html'
+TEMPLATE_FILE_NAME = "template.html"
 
 # Template css file name
-TEMPLATE_FILE_CSS = 'template.css'
+TEMPLATE_FILE_CSS = "template.css"
 
 # Config file
-CONFIG_FILE = DIRECTORY / 'config.ini'
+CONFIG_FILE = DIRECTORY / "config.ini"
 
 
 # Get sections from config file, return a list, porvid the user to choose.
@@ -46,6 +46,7 @@ def get_sections():
     cfg.read(CONFIG_FILE)
     sections = cfg.sections()
     return sections
+
 
 # Get config in the section which user choose. It have decided the style of the html and pdf file.
 
@@ -56,20 +57,21 @@ def get_config(section):
     config = dict(cfg.items(section))
     return config
 
+
 # Convert markdown file to html file.
 
 
 def md2html(md_file, output_dir, config):
-    markdown_css_path = STYLES_DIR / config['markdown_css']
-    markdown_custom_css_path = STYLES_DIR / config['markdown_custom_css']
-    template_path = TEMPLATE_DIR / config['template_name'] / TEMPLATE_FILE_NAME
-    template_css_path = TEMPLATE_DIR / config['template_name'] / TEMPLATE_FILE_CSS
+    markdown_css_path = STYLES_DIR / config["markdown_css"]
+    markdown_custom_css_path = STYLES_DIR / config["markdown_custom_css"]
+    template_path = TEMPLATE_DIR / config["template_name"] / TEMPLATE_FILE_NAME
+    template_css_path = TEMPLATE_DIR / config["template_name"] / TEMPLATE_FILE_CSS
 
     md_file_dir = md_file.parent
 
-    styles_dir = md_file_dir / 'styles'
+    styles_dir = md_file_dir / "styles"
     # html_file = output_dir / (md_file.stem + '.html')
-    html_file = os.path.join(md_file_dir, md_file.stem + '.html')
+    html_file = os.path.join(md_file_dir, md_file.stem + ".html")
 
     if not Path.exists(styles_dir):
         Path.mkdir(styles_dir)
@@ -77,85 +79,109 @@ def md2html(md_file, output_dir, config):
     for style_file in markdown_css_path, markdown_custom_css_path, template_css_path:
         copy(style_file, styles_dir)
 
-    extensions = ['extra', 'meta', 'nl2br',
-                  'sane_lists', 'smarty', 'toc', 'wikilinks']
+    extensions = ["extra", "meta", "nl2br", "sane_lists", "smarty", "toc", "wikilinks"]
 
-    with open(md_file, 'r', encoding="utf-8") as mf:
-        md = markdown.Markdown(output_format='html5', extensions=extensions)
+    with open(md_file, "r", encoding="utf-8") as mf:
+        md = markdown.Markdown(output_format="html5", extensions=extensions)
         html = md.convert(mf.read())
         meta_data = md.Meta
-        meta_data = {k: ' '.join(v) for k, v in meta_data.items()}
+        meta_data = {k: " ".join(v) for k, v in meta_data.items()}
 
-    with open(template_path, 'r', encoding="utf-8") as tf:
+    with open(template_path, "r", encoding="utf-8") as tf:
         template_html = Template(tf.read())
         d = {**config, **meta_data}
         full_html = template_html.safe_substitute(d, content=html)
 
-    with open(html_file, 'w', encoding="utf-8") as hf:
+    with open(html_file, "w", encoding="utf-8") as hf:
         hf.write(full_html)
 
     return html_file, styles_dir, meta_data
 
+
 # Convert html file to pdf file.
+
 
 def html2pdf(html_file, pdf_file):
     options = {
-        'encoding': "UTF-8",
-        'margin-top': '15mm',
-        'margin-right': '15mm',
-        'margin-bottom': '15mm',
-        'margin-left': '15mm',
-        'page-size': 'A4',
-        'custom-header': [
-            ('Accept-Encoding', 'gzip')
+        "encoding": "UTF-8",
+        "margin-top": "15mm",
+        "margin-right": "15mm",
+        "margin-bottom": "15mm",
+        "margin-left": "15mm",
+        "page-size": "A4",
+        "custom-header": [("Accept-Encoding", "gzip")],
+        "cookie": [
+            ("cookie-empty-value", '""'),
+            ("cookie-name1", "cookie-value1"),
+            ("cookie-name2", "cookie-value2"),
         ],
-        'cookie': [
-            ('cookie-empty-value', '""'),
-            ('cookie-name1', 'cookie-value1'),
-            ('cookie-name2', 'cookie-value2'),
-        ],
-        'footer-font-size': '6',
-        'footer-font-name': 'Times-Roman',
-        'footer-center': '[page]/[topage]',
-        'footer-spacing': '5',
-        'enable-local-file-access': None
+        "footer-font-size": "6",
+        "footer-font-name": "Times-Roman",
+        "footer-center": "[page]/[topage]",
+        "footer-spacing": "5",
+        "enable-local-file-access": None,
     }
     pdfkit.from_file(html_file, pdf_file, options=options)
     return
+
 
 # Get parameters from the command line using click
 
 sections = get_sections()
 
+
 @click.command()
-@click.option('-t', '--template', default='DEFAULT', show_default='True', help=f'select a HTML template: {sections}')
-@click.argument('md_file', type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True), required=True, nargs=1)
-@click.argument('output_dir', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True), required=False, nargs=1)
+@click.option(
+    "-t",
+    "--template",
+    default="DEFAULT",
+    show_default="True",
+    help=f"select a HTML template: {sections}",
+)
+@click.argument(
+    "md_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    required=True,
+    nargs=1,
+)
+@click.argument(
+    "output_dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+    required=False,
+    nargs=1,
+)
 def main(template, md_file, output_dir):
     md_file = Path(md_file).resolve()
     if not output_dir:
         output_dir = md_file.parent
     else:
         output_dir = Path(output_dir).resolve()
-    if template not in sections and template != 'DEFAULT':
-        print(f'Error: template {template} is not exist.')
+    if template not in sections and template != "DEFAULT":
+        print(f"Error: template {template} is not exist.")
         sys.exit()
-    
+
     config = get_config(template)
     html_file, styles_dir, meta_data = md2html(md_file, output_dir, config)
     # pdf_file = output_dir / (md_file.stem + '.pdf')
-    name_parts = [meta_data['role'], meta_data['document'], meta_data['date'], meta_data['author'], meta_data['subject']]
+    name_parts = [
+        meta_data["role"],
+        meta_data["document"],
+        meta_data["date"],
+        meta_data["author"],
+        meta_data["subject"],
+    ]
     # pdf_file = output_dir / ('-'.join(name_parts) + '.pdf')
-    pdf_file = os.path.join(output_dir, '-'.join(name_parts) + '.pdf')
+    pdf_file = os.path.join(output_dir, "-".join(name_parts) + ".pdf")
 
-    if config['generate_pdf']:
+    if config["generate_pdf"]:
         html2pdf(html_file, pdf_file)
 
-    if config['generate_html'] == 'False':
-        print(f'Delete {html_file}')
+    if config["generate_html"] == "False":
+        print(f"Delete {html_file}")
         os.remove(html_file)
-        print(f'Delete {styles_dir}')
+        print(f"Delete {styles_dir}")
         rmtree(styles_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
